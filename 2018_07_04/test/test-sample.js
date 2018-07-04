e328b45b9aeb1e222d830163cd23a51bae66039c
mass-min <koyod111@gmail.com>
2018-07-04 18:17:03 +0700

    add script for self writing

---
+++ 2018_07_04/test/test-sample.js
@@ -0,0 +1,19 @@
+const chai = require('chai');
+const expect = chai.expect;
+const sample = require('../scripts/sample');
+
+chai.should()
+
+describe('sample', function() {
+    describe('#add', function() {
+        it('should return 3 when args are 1 and 2', function() {
+
+        });
+        it('should return 0.4 when args are 0.1 and 0.3', function() {
+
+        });
+        it('should throw error when args include null', function() {
+
+        });
+    });
+});
